02f7652cb02ec4663cf9d06599634cb1a0f91e9a
bnjpcson <pecsonbenjiea@gmail.com>
2025-08-22 16:58:05 +0800

    Added a projects card in project page, also added an animation scrolling effect

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,7 +1,9 @@
+import Projects from "./Projects";
+
 export default function ProjectsPage() {
   return (
     <div className="mt-16 z-0">
-      <div className="flex flex-col h-[75vh] w-full bg-background py-10 shadow-md  items-center">
+      <div className="flex flex-col w-full bg-background py-10 shadow-md  items-center">
         <div className="flex flex-col w-full gap-5 text-center max-w-2xl px-10">
           <h1 className="text-4xl font-bold text-center w-full">
             <span className="text-primary">My</span> Projects
@@ -11,16 +13,8 @@ export default function ProjectsPage() {
             built with cutting-edge technologies and modern design principles.
           </p>
         </div>
-        {/* <div className="w-full px-5 sm:px-14 md:px-20 lg:px-32 py-5">
-          <div className="grid grid-cols-3 gap-5 place-items-center bg-blue-200">
-            <div className="w-40 h-40 bg-amber-300">123</div>
-            <div className="w-40 h-40 bg-amber-300">123</div>
-            <div className="w-40 h-40 bg-amber-300">123</div>
-            <div className="w-40 h-40 bg-amber-300">123</div>
-            <div className="w-40 h-40 bg-amber-300">123</div>
-            <div className="w-40 h-40 bg-amber-300">123</div>
-          </div>
-        </div> */}
+
+        <Projects />
       </div>
     </div>
   );
